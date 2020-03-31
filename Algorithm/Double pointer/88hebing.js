59e3edff3fb22ebbd113fa6fb65bc37e83162eaa
huangmei123 <1593702111@qq.com>
2020-03-31 14:49:15 +0800

    88

---
+++ Algorithm/Double pointer/88hebing.js
@@ -0,0 +1,20 @@
+var merge = function(nums1, m, nums2, n) {
+    var index1 = m - 1;
+    var index2 = n - 1;
+    var indexMerge = m + n - 1;
+
+
+
+        while (index1 >= 0 || index2 >= 0) {
+            if (index1 < 0) {
+                nums1[indexMerge--] = nums2[index2--];
+            } else if (index2 < 0) {
+                nums1[indexMerge--] = nums1[index1--];
+            } else if (nums1[index1] > nums2[index2]) {
+                nums1[indexMerge--] = nums1[index1--];
+            } else {
+                nums1[indexMerge--] = nums2[index2--];
+            }
+        }
+
+};

d81f664d1a7d706ebc53117934a1e53ed833516f
huangmei123 <1593702111@qq.com>
2020-05-12 09:23:31 +0800

    MinStack

---
+++ everyday/155.js
@@ -0,0 +1,26 @@
+//Min stack
+//栈的特性 先进后出
+//使用一个辅助栈 与元素同步插入与删除，用于存储与每一个元素对应的最小值
+var MinStack = function() {
+    this.x_stack = [];
+    this.min_stack = [Infinity];
+};
+
+MinStack.prototype.push = function(x) {
+    this.x_stack.push(x);
+    this.min_stack.push(Math.min(this.min_stack[this.min_stack.length - 1], x));
+};
+
+MinStack.prototype.pop = function() {
+    this.x_stack.pop();
+    this.min_stack.pop();
+};
+
+MinStack.prototype.top = function() {
+    return this.x_stack[this.x_stack.length - 1];
+};
+
+MinStack.prototype.getMin = function() {
+    return this.min_stack[this.min_stack.length - 1];
+};
+
